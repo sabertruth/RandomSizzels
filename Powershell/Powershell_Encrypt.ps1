84cd1f0c9c2f527f95c6af2bd6f6964c45901721
sabertruth <48374071+sabertruth@users.noreply.github.com>
2024-07-29 17:52:26 +1000

    pp

---
+++ Powershell/Powershell_Encrypt.ps1
@@ -0,0 +1,3 @@
+﻿$File = "C:\Users\Metal Gear\Documents\GitHub\RandomSizzels\Powershell\secure_encrypt.txt"
+$Password = "P@ssword1" | ConvertTo-SecureString -AsPlainText -Force
+$Password | ConvertFrom-SecureString | Out-File $File
---
+++ Powershell/Powershell_deCreep.ps1
@@ -0,0 +1,9 @@
+﻿# Define the file path
+$File = "C:\Users\Metal Gear\Documents\GitHub\RandomSizzels\Powershell\secure_encrypt.txt"
+
+# Read and decrypt the password
+$EncryptedPassword = Get-Content -Path $File | ConvertTo-SecureString
+$PlainTextPassword = [System.Runtime.InteropServices.Marshal]::PtrToStringAuto([System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($EncryptedPassword))
+
+# Output the decrypted password
+Write-Output "Decrypted Password: $PlainTextPassword"
